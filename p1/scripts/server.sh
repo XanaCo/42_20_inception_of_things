c691772cfd2d71469c71a9aca27bf7174fbae94c
Ana <a.colmenero@gmail.com>
2024-12-01 18:28:48 +0100

    Agent does not connect to the correct address, could not find a solution yet

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -7,10 +7,15 @@ sudo apt-get update
 sudo apt-get install curl -y
 
 # Modify the K3s environment variables
-export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san serverS --node-ip 192.168.56.110 --bind-address=192.168.56.110 --advertise-address=192.168.56.110 "
+export INSTALL_K3S_EXEC="server \
+--write-kubeconfig-mode=644 \
+--node-ip 192.168.56.110 \
+--bind-address=192.168.56.110 \
+--advertise-address=192.168.56.110 
+--flannel-iface=eth1"
 
 # Install K3s Controller Plane
-curl -sfL https://get.k3s.io | sh -
+curl -sfL https://get.k3s.io | sh -s -
 
 # Get the K3s server token
 sudo cat /var/lib/rancher/k3s/server/token > /vagrant/server_token.env
--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -7,10 +7,14 @@ sudo apt-get update
 sudo apt-get install curl -y
 
 # Modify the K3s environment variables
-export INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 -t $(cat /vagrant/server_token.env) --node-ip=192.168.56.111"
+export INSTALL_K3S_EXEC="agent \
+--server https://192.168.56.110:6443 \
+--token-file "/vagrant/server_token.env" \
+--node-ip=192.168.56.111
+--flannel-iface=eth1"
 
 # Install K3s Worker Node
-curl -sfL https://get.k3s.io | sh -
+curl -sfL https://get.k3s.io | sh -s -
 
 # Remove the token file
 sudo rm /vagrant/server_token.env
