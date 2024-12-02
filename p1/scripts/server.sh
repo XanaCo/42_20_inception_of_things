e47296b791499d9a86030fb55ed4fd2ee432203b
Ana <a.colmenero@gmail.com>
2024-12-02 17:53:48 +0100

    Beautify + testing

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -1,23 +1,60 @@
 #!bin/bash
 
-echo "SERVER NODE CONFIGURATION"
+B_GREEN="\033[0;32m"
+RED="\033[0;31m"
+B_RED="\033[1;31m"
+RESET="\033[0m"
 
-# Install curl in the server node
-sudo apt-get update
-sudo apt-get install curl -y
+echo "${B_GREEN}. . . SERVER CONFIGURATION SCRIPT . . .${RESET}"
+
+# Preinstall Config in the server node
+if sudo DEBIAN_FRONTEND=noninteractive apt-get update -y && \
+sudo apt-get upgrade -y && \
+sudo apt-get install curl -y;
+then
+    echo "${B_GREEN}Preinstallation Succeded!${RESET}"
+else
+    echo "${B_RED}Error: Preinstallation Failed${RESET}"
+    exit 1
+fi
 
 # Modify the K3s environment variables
-export INSTALL_K3S_EXEC="server \
+if export INSTALL_K3S_EXEC="
 --write-kubeconfig-mode=644 \
---node-ip 192.168.56.110 \
 --bind-address=192.168.56.110 \
---advertise-address=192.168.56.110 
---flannel-iface=eth1"
+--node-ip 192.168.56.110 \
+--tls-san $(hostname) \
+--advertise-address=192.168.56.110"; then
+    echo "${B_GREEN}K3s Environment Variables Set!${RESET}"
+else
+    echo "${B_RED}Error: K3s Environment Variables Not Set!${RESET}"
+    exit 1
+fi
 
 # Install K3s Controller Plane
-curl -sfL https://get.k3s.io | sh -s -
+if curl -sfL https://get.k3s.io | sh -; then
+    echo "${B_GREEN}K3s Controller Plane Installed!${RESET}"
+else
+    echo "${B_RED}Error: K3s Controller Plane Not Installed!${RESET}"
+    exit 1
+fi
+
+if sudo mkdir -p ~/.kube && \
+sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config && \
+sudo chmod 600 ~/.kube/config && \
+sudo chown $(id -u):$(id -g) ~/.kube/config; then
+    echo "${B_GREEN}K3s Kubeconfig File Copied!${RESET}"
+else
+    echo "${B_RED}Error: K3s Kubeconfig File Not Copied!${RESET}"
+    exit 1
+fi
 
 # Get the K3s server token
-sudo cat /var/lib/rancher/k3s/server/token > /vagrant/server_token.env
+if sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/scripts/; then
+    echo "${B_GREEN}K3s Server Token Copied!${RESET}"
+else
+    echo "${B_RED}Error: K3s Server Token Not Copied!${RESET}"
+    exit 1
+fi
 
-echo "K3s Server Node is Ready!"
+echo "${B_GREEN}. . . SERVER IS READY . . .${RESET}"
--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -1,22 +1,47 @@
-#!bin/bash
+#!/bin/bash
 
-echo "WORKER NODE CONFIGURATION"
+GREEN="\033[0;32m"
+B_GREEN="\033[1;32m"
+RED="\033[0;31m"
+B_RED="\033[1;31m"
+RESET="\033[0m"
 
-# Install curl in the worker node
-sudo apt-get update
-sudo apt-get install curl -y
+echo "${B_GREEN}. . . WORKER CONFIGURATION SCRIPT . . .${RESET}"
+
+# Preinstall Config in the worker node
+if sudo DEBIAN_FRONTEND=noninteractive apt-get update -y && \
+sudo apt-get upgrade -y && \
+sudo apt-get install curl -y;
+then
+    echo "${GREEN}Preinstallation Succeded!${RESET}"
+else
+    echo "${B_RED}Error: Preinstallation Failed${RESET}"
+    exit 1
+fi
 
 # Modify the K3s environment variables
-export INSTALL_K3S_EXEC="agent \
+if export INSTALL_K3S_EXEC="agent \
 --server https://192.168.56.110:6443 \
---token-file "/vagrant/server_token.env" \
---node-ip=192.168.56.111
---flannel-iface=eth1"
+--token-file "/vagrant/scripts/node-token" \
+--node-ip=192.168.56.111"; then
+    echo "${GREEN}K3s Environment Variables Set!${RESET}"
+else
+    echo "${B_RED}Error: K3s Environment Variables Not Set!${RESET}"
+    exit 1
+fi
 
 # Install K3s Worker Node
-curl -sfL https://get.k3s.io | sh -s -
+if curl -sfL https://get.k3s.io | sh -; then
+    echo "${GREEN}K3s Worker Node Installed!${RESET}"
+else
+    echo "${B_RED}Error: K3s Worker Node Not Installed!${RESET}"
+    exit 1
+fi
 
-# Remove the token file
-sudo rm /vagrant/server_token.env
-
-echo "K3s Worker Node is Ready!"
+# Remove the token file (for security)
+if sudo rm /vagrant/scripts/node-token; then
+    echo "${GREEN}K3s Server Token Removed!${RESET}"
+else
+    echo "${B_RED}Error: K3s Server Token Not Removed!${RESET}"
+    exit 1
+fi
