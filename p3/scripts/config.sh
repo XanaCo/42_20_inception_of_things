d8b7102ee945cd1bb32486173849f5b2eb28e247
Ana <a.colmenero@gmail.com>
2024-12-05 20:24:35 +0100

    p3 script started

--- p3/scripts/config.sh
+++ p3/scripts/config.sh
@@ -8,7 +8,6 @@ echo "${B_GREEN}. . . CONFIGURATION SCRIPT . . .${RESET}"
 
 # Preinstall Config
 if sudo apt-get update -y && \
-sudo apt-get upgrade -y && \
 sudo apt-get install curl -y && \
 echo "alias k='kubectl'" >> /etc/profile.d/00-aliases.sh;
 then
@@ -17,5 +16,50 @@ else
     echo "${B_RED}Error: Preinstallation Already Done${RESET}"
 fi
 
-# install k3d if not installed
-# install docker if not installed
+# 00.Install K3d Requisites: 
+# A. kubeclt:
+# https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/#install-kubectl-binary-with-curl-on-linux
+
+# Install and test validate checksum
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+
+# Install kubectl and test to verify installation
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+    # # If you don't have root access, you may need to use a different directory: (A TESTER)
+    # chmod +x kubectl
+    # mkdir -p ~/.local/bin
+    # mv ./kubectl ~/.local/bin/kubectl
+    # # and then append (or prepend) ~/.local/bin to $PATH
+    # export PATH=$PATH:~/.local/bin
+
+# B. Docker
+# https://get.docker.com/
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+# 01.Install K3d
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+k3d --version
+# Create a cluster
+k3d cluster create dev-app
+
+# 02.Install ArgoCD
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+# wait for pods
+kubectl wait --for=condition=Ready pods --all -n argocd
+# get secret
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d 
+# apply yml
+kubectl apply -f /confs/argocd/deploy.yml
+
+kubectl create namespace dev
+
+# Deploy
+
