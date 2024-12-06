73ade8ccfa2f2b4a77a2978ae00be08e3b9d0d88
Ana COLMENERO <ancolmen@made-f0Br2s5.clusters.42paris.fr>
2024-12-06 17:38:14 +0100

    script maj

--- p3/scripts/VMcleanup.sh
+++ p3/scripts/VMcleanup.sh
@@ -1,14 +1,18 @@
 #!bin/bash
 
-sudo rm /usr/local/bin/kubectl &&
-rm kubectl &&
-sudo apt-get purge -y docker docker-engine docker.io containerd runc &&
-sudo apt-get autoremove -y &&
-sudo rm -rf /var/lib/docker &&
-sudo rm -f /usr/local/bin/k3d &&
-k3d cluster delete dev-app &&
-kubectl delete namespace argocd &&
-kubectl delete namespace dev &&
-rm -f get-docker.sh &&
-sudo apt-get clean &&
+sudo rm /usr/local/bin/kubectl
+rm kubectl
+rm kubectl.sha256
+rm get-docker.sh
+sudo docker rm -f $(docker ps -a -q)
+sudo docker rmi $(docker images -q)
+sudo docker system prune
+sudo docker volume prune
+sudo rm -rf /var/lib/docker
+sudo k3d cluster delete dev-app
+sudo rm -f /usr/local/bin/k3d
+sudo kubectl delete namespace argocd
+sudo kubectl delete namespace dev
+
+sudo apt-get clean 
 sudo apt-get autoremove -y
--- p3/scripts/VMstartup.sh
+++ p3/scripts/VMstartup.sh
@@ -1,14 +1,14 @@
 #!bin/bash
 
-sudo usermod -aG sudo vboxuser &&
-su vboxuser &&
-sudo apt-get update -y && 
-sudo apt-get upgrade && 
-sudo apt-get install curl -y && 
-sudo apt-get install git && 
-echo "alias k='kubectl'" >> /etc/profile.d/00-aliases.sh &&
-ssh-keygen -t ed25519 -C "ancolmen@student.42.fr" &&
-eval "$(ssh-agent -s)" &&
-ssh-add ~/.ssh/id_ed25519 &&
+sudo usermod -aG sudo vboxuser
+su vboxuser
+sudo apt-get update -y 
+sudo apt-get upgrade 
+sudo apt-get install curl -y 
+sudo apt-get install git 
+echo "alias k='kubectl'" >> /etc/profile.d/00-aliases.sh
+ssh-keygen -t ed25519 -C "ancolmen@student.42.fr"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
 cat ~/.ssh/id_ed25519.pub
 
