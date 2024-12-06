4f8d4bc809f77bed4246ce7b8f9d074768f463b4
Ana COLMENERO <ancolmen@made-f0Br2s5.clusters.42paris.fr>
2024-12-06 16:30:00 +0100

    Added startup script

---
+++ p3/scripts/VMstartup.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+sudo usermod -aG sudo vboxuser &&
+sudo apt-get update -y && 
+sudo apt-get upgrade && 
+sudo apt-get install curl -y && 
+sudo apt-get install git && 
+echo "alias k='kubectl'" >> /etc/profile.d/00-aliases.sh &&
+ssh-keygen -t ed25519 -C "ancolmen@student.42.fr" &&
+eval "$(ssh-agent -s)" &&
+ssh-add ~/.ssh/id_ed25519 &&
+cat ~/.ssh/id_ed25519.pub | xclip -selection clipboard
+
+
+git add .
