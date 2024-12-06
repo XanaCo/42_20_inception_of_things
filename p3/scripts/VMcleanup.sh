4a15b9d5f4ba3e030492c830b372402fc76c083f
Ana COLMENERO <ancolmen@made-f0Br2s5.clusters.42paris.fr>
2024-12-06 17:01:33 +0100

    Added cleanup script

---
+++ p3/scripts/VMcleanup.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+sudo rm /usr/local/bin/kubectl &&
+rm ~/.local/bin/kubectl &&
+sudo apt-get purge -y docker docker-engine docker.io containerd runc &&
+sudo apt-get autoremove -y &&
+sudo rm -rf /var/lib/docker &&
+sudo rm -f /usr/local/bin/k3d &&
+k3d cluster delete dev-app &&
+kubectl delete namespace argocd &&
+kubectl delete namespace dev &&
+rm -f get-docker.sh &&
+sudo apt-get clean &&
+sudo apt-get autoremove -y
