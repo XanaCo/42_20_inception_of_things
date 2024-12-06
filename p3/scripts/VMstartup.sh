9dc915ef1b2fd438462e19887ed2ecd0e239b3f2
Ana COLMENERO <ancolmen@made-f0Br2s5.clusters.42paris.fr>
2024-12-06 18:22:31 +0100

    more corrections

--- p3/scripts/VMstartup.sh
+++ p3/scripts/VMstartup.sh
@@ -1,7 +1,9 @@
 #!bin/bash
 
-sudo usermod -aG sudo vboxuser
-su vboxuser &&
+# RUN on root or do this before!
+# sudo usermod -aG sudo vboxuser
+# su vboxuser
+
 sudo apt-get update -y 
 sudo apt-get upgrade 
 sudo apt-get install curl -y 
@@ -10,13 +12,8 @@ echo "alias k='kubectl'" >> /etc/profile.d/00-aliases.sh
 ssh-keygen -t ed25519 -C "ancolmen@student.42.fr"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
-
 sudo curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt-get update -y
 sudo apt-get install code -y
-
 cat ~/.ssh/id_ed25519.pub
-
-
-
