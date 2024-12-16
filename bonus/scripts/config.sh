c17b09825401717f0cf43f471e99a67cfd45771d
vboxuser <vboxuser@IoT-tmp.myguest.virtualbox.org>
2024-12-16 17:50:53 +0100

    test

--- bonus/scripts/config.sh
+++ bonus/scripts/config.sh
@@ -42,11 +42,40 @@ echo "${B_GREEN}. . . K3D INSTALL${RESET}"
 sudo curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 sudo k3d --version
 sudo k3d cluster create dev-app --wait
+sudo kubectl create namespace argocd
+sudo kubectl create namespace gitlab
+sudo kubectl create namespace dev
+
+# Install Helm
+echo "${B_GREEN}. . . HELM INSTALL${RESET}"
+sudo curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+sudo chmod 700 get_helm.sh
+sudo ./get_helm.sh
+
+# Get gitlab with helm
+echo "${B_GREEN}. . . GITLAB HELM INSTALL${RESET}"
+sudo helm repo add gitlab https://charts.gitlab.io
+sudo helm repo update gitlab
+sudo helm upgrade --install gitlab gitlab/gitlab \
+  -n gitlab \
+  -f https://gitlab.com/gitlab-org/charts/gitlab/raw/master/examples/values-minikube-minimum.yaml \
+  --set global.hosts.domain=gitlab.example.com \
+  --set global.hosts.externalIP= \
+  --set global.hosts.https=false \
+  --timeout 1000s
+
+# Get gitlab pods
+echo "${B_GREEN}. . . WAIT FOR PODS GITLAB TO BE READY${RESET}"
+sudo kubectl wait --for=condition=Ready pods --all -n gitlab
+sudo kubectl -n gitlab get pods
+
+# Get gitlab password
+echo "${B_GREEN}. . . GET SECRET AND PORT-FORWARD GITLAB${RESET}"
+sudo kubectl -n gitlab get secret gitlab-gitlab-initial-root-password -o jsonpath="{.data.password}" | base64 -d > .gitlab_pass
+sudo kubectl port-forward svc/gitlab-webservice-default -n gitlab 80:8181 >/dev/null 2>&1 &
 
 # 02.Install ArgoCD
 echo "${B_GREEN}. . . ARGOCD INSTALL${RESET}"
-sudo kubectl create namespace argocd
-sudo kubectl create namespace dev
 sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 sleep 10
@@ -54,23 +83,9 @@ echo "${B_GREEN}. . . WAIT FOR PODS ARGOCD TO BE READY${RESET}"
 sudo kubectl wait --for=condition=Ready pods --all -n argocd
 sudo kubectl -n argocd get pods
 
-# 03.Deploy ArgoCD + app
-echo "${B_GREEN}. . . DEPLOY APP${RESET}"
-sudo kubectl apply -n argocd -f ../confs/argocd/deploy.yml
-
-# 04.Redirections and port access
+# 03.Redirections and port access
 sleep 10
 echo "${B_GREEN}. . . GET SECRET AND PORT-FORWARD ARGOCD${RESET}"
 sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d > .argocd_pass
 sudo kubectl port-forward svc/argocd-server -n argocd 8080:443 > /dev/null 2>&1 &
 
-echo "${B_GREEN}. . . WAIT FOR PODS DEV TO BE READY${RESET}"
-sudo kubectl wait --for=condition=Ready pods --all -n dev
-sudo kubectl -n dev get pods
-
-# while true
-# do
-#   sudo kubectl port-forward svc/wil-service -n dev 8888:8888
-#   echo "Port forwarding stopped or failed. Retrying in 5 seconds..."
-#   sleep 5
-# done
